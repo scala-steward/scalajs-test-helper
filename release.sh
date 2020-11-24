c879a354a18fb4a7f7c14860af8d83678768e7db
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2020-11-24 12:35:33 +0900

    Update release script

--- release.sh
+++ release.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-export SCALAJS_VERSION=0.6.32
-sbt clean +test +publishSigned sonatypeBundleUpload sonatypeReleaseAll
-export SCALAJS_VERSION=1.0.0
 sbt clean +test +publishSigned sonatypeBundleUpload sonatypeReleaseAll
